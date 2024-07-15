d587e4ba34d9e739c83a7438f2eee5d61dc4c97d
ArunVijay-PB <arun.vijay@pinboardconsulting.com>
2024-07-15 15:58:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <h1>Pinboard<h1/>
-      </header>
+      <h1>Pinboard</h1>
     </div>
   );
 }
